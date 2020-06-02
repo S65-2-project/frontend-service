d8a53a45802ec5d59f6ddc72eefff023acb23b64
Floris Feddema <floris1996@hotmail.com>
2020-06-02 16:19:15 +0200

    S65-197: console logs weggehaald, probleem doet zich alleen voor door caching tijdens development

--- src/dapp/DAppOfferDetails.tsx
+++ src/dapp/DAppOfferDetails.tsx
@@ -20,25 +20,23 @@ export const DAppOfferDetails = (props: any) => {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin" : "http:localhost:3000"
                 },
                 mode: "cors",
                 cache: "default"
             };
             let idRequest: string = "/"+id;
-            let response: Response = await fetch(config.SERVICES.DAppOfferGet+idRequest, options);
+            let response: Response = await fetch(config.SERVICES.DAPP+idRequest, options);
             let body = await response.text();
             if (response.status === 200) {
-                console.log(body)
                 return JSON.parse(body); //returns type DAppOffer if backend is consistent.
             } else {
                 throw new Error(body);
             }
+
         };
 
         const loadHtml = async (id: string) => {
             try {
-
                 let details: DAppOffer = await getInformation(id);
                 const participants = details.delegatesCurrentlyInOffer.map(
                     (delegate: User) =>
