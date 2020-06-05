c8f337c2a1867b929065a35e13ab792c0cbfe1e8
Bstijn <baltessen.stijn@gmail.com>
2020-06-05 10:37:26 +0200

    s65-178: review changes that are requested resolved

--- src/dapp/types/UpdateDAppOfferModel.ts
+++ src/dapp/types/UpdateDAppOfferModel.ts
@@ -1,14 +1,6 @@
 import {User} from "./CreateDAppOfferModel";
 
 export interface UpdateDAppOfferModel{
-//     [Required] public string Title { get; set; }
-// [Required] public string Description { get; set; }
-// [Required] public int OfferLengthInMonths { get; set; }
-// [Required] public int LiskPerMonth { get; set; }
-// [Required] public int DelegatesNeededForOffer { get; set; }
-// [Required] public string Region { get; set; }
-// [Required] public DateTime DateStart { get; set; }
-// [Required] public DateTime DateEnd { get; set; }
     Title: string
     Provider: User,
     Description: string,
