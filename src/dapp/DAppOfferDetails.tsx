26e7d91c41efc5d7159461fcc188924acc85f39e
Luc Urlings <urlingsluc@gmail.com>
2020-06-15 11:56:19 +0200

    feature/S65-226: Bugfix: added modal, restructured code, fixed deleteDApp

--- src/dapp/DAppOfferDetails.tsx
+++ src/dapp/DAppOfferDetails.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import {DAppOffer} from './types/DAppOffer';
 import config from "../config.json";
 import {useParams, withRouter} from "react-router";
-import {Alert, Button, Form, ListGroup} from "react-bootstrap";
+import {Alert, Button, Form, ListGroup, Modal} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {User} from "./types/CreateDAppOfferModel";
@@ -13,6 +13,69 @@ export const DAppOfferDetails = (props: any) => {
 
     const [information, setInformation] = React.useState(<div/>)
     const [error, setError] = React.useState(<div/>)
+    const [showModal, setShowModal] = React.useState(false);
+
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
+
+
+    const Dialogue = (props: any) => {
+        return (
+            <Modal
+                {...props} aria-labelledby="contained-modal-title-vcenter" centered>
+
+                <Modal.Header
+                    closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">Weet je zeker dat je deze DApp wil
+                        verwijderen?</Modal.Title> </Modal.Header>
+                <Modal.Body>
+                    <Button variant="secondary" onClick={props.handleClose}>
+                        Nee
+                    </Button>
+                    <Button variant="primary" onClick={props.deleteDappOffer}>
+                        Ja
+                    </Button>
+                </Modal.Body>
+            </Modal>);
+    };
+
+    const deleteOffer = async () => {
+        let id = props.match.params.id
+        if (id) {
+            try {
+                await DeleteOffer(id)
+                props.history.push("/dapp-overview")
+            } catch (e) {
+                setError(<Alert variant={"danger"} onClick={() => setError(<div/>)}>{e.message}</Alert>)
+                return;
+            }
+        }
+    }
+
+    const DeleteOffer = async (id: string) => {
+        let options: RequestInit = {
+            method: "DELETE",
+            headers: {
+                'Authorization': "Bearer " + props.auth.User.token,
+                "Content-Type": "application/json"
+            },
+            mode: "cors",
+            cache: "default"
+        }
+
+        let response = await fetch(config.SERVICES.DAPP + "/" + id, options);
+        let data = await response.text();
+
+        if (response.status !== 200) {
+            throw new Error(JSON.stringify(data));
+        } else {
+            return true
+        }
+    };
+
+    const CanDelete = (provider: any) => {
+        return props.auth.User.id === provider.id;
+    }
 
 
     useEffect(() => {
@@ -25,8 +88,8 @@ export const DAppOfferDetails = (props: any) => {
                 mode: "cors",
                 cache: "default"
             };
-            let idRequest: string = "/"+id;
-            let response: Response = await fetch(config.SERVICES.DAPP+idRequest, options);
+            let idRequest: string = "/" + id;
+            let response: Response = await fetch(config.SERVICES.DAPP + idRequest, options);
             let body = await response.text();
             if (response.status === 200) {
                 return JSON.parse(body); //returns type DAppOffer if backend is consistent.
@@ -34,39 +97,6 @@ export const DAppOfferDetails = (props: any) => {
                 throw new Error(body);
             }
 
-        };
-
-        const deleteOffer = async (id: string) => {
-            if(id) {
-                try {
-                    await DeleteOffer(id)
-                    props.history.push("/dappoffer")
-                }
-                catch (e) {
-                    setError(<Alert variant={"danger"} onClick={() => setError(<div/>)}>{e.message}</Alert>)
-                    return;
-                }
-            }
-        }
-
-        const DeleteOffer  = async (id: string): Promise<boolean> => {
-            let options: RequestInit = {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                mode: "cors",
-                cache: "default"
-            }
-            let idRequest: string = "/"+id;
-            let response: Response = await fetch(config.SERVICES.DAPP+idRequest, options);
-            if (response.status === 200) {
-
-                return true;
-            } else {
-                let text = await response.text();
-                throw new Error(text);
-            }
         };
 
         const loadHtml = async (id: string) => {
@@ -88,7 +118,9 @@ export const DAppOfferDetails = (props: any) => {
                     <Form style={{marginTop: '20px'}}>
                         <h3><Form.Group>
                             <Form.Label>Offer made
-                                by: <Link to={"/profile/" + details.provider.id}><strong>{details.provider.Name}</strong></Link>.</Form.Label>
+                                by: <Link
+                                    // @ts-ignore
+                                    to={{pathname: "/profile/" + details.provider.id}}>{details.provider.name}</Link>.</Form.Label>
                         </Form.Group>
                         </h3>
                         <Form.Group>
@@ -100,23 +132,23 @@ export const DAppOfferDetails = (props: any) => {
                                 for <strong>{details.offerLengthInMonths}</strong> months.</Form.Label>
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Reward will be <strong>{details.liskPerMonth}</strong> Lisk a month.</Form.Label>
+                            <Form.Label>Reward will be <strong>{details.liskPerMonth}</strong> Lisk a
+                                month.</Form.Label>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Delegates need in this offer: <strong>{details.delegatesNeededForOffer}</strong>.</Form.Label>
                         </Form.Group>
                         {
                             details.delegatesCurrentlyInOffer.length !== 0
-                            ?   <Form.Group>
+                                ? <Form.Group>
                                     <Form.Label>Delegates currently in offer:</Form.Label>
                                     <Form.Group>
                                         {participantBlock}
                                     </Form.Group>
                                 </Form.Group>
-                            :   <Form.Group>
+                                : <Form.Group>
                                     <Form.Label>Delegates currently in offer: none</Form.Label>
                                 </Form.Group>
-
                         }
 
                         <Form.Group>
@@ -125,7 +157,13 @@ export const DAppOfferDetails = (props: any) => {
                         <Form.Group>
                             <Form.Label>Date end : <strong>{details.dateEnd}</strong> </Form.Label>
                         </Form.Group>
-                        <Button onClick={() => deleteOffer(id)}>Delete</Button>
+                        {
+                            CanDelete(details.provider)
+                                ? <>
+                                    <Button onClick={handleShow}>Delete Offer</Button>
+                                </>
+                                : <></>
+                        }
                     </Form>
                 )
             } catch (e) {
@@ -136,13 +174,14 @@ export const DAppOfferDetails = (props: any) => {
             loadHtml(id)
         }
 
-
     }, [id, props.history]);
 
     return (
         <div>
             {error}
             {information}
+            <Dialogue show={showModal} deleteDappOffer={deleteOffer} handleClose={handleClose}
+                      onHide={() => setShowModal(false)}/>
         </div>
     )
 };
