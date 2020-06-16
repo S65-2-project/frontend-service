82fa59ceefc89481e3516cd26daff4595b869dad
Joeri Berman <joeriberman@hotmail.com>
2020-06-16 13:56:59 +0200

    S65-256: Added https to url

--- K6Scripts/cloudtest.js
+++ K6Scripts/cloudtest.js
@@ -3,14 +3,20 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  duration: '1m',
-  vus: 50,
+  duration: '20s',
+  vus: 200,
+  distribution: {
+    scenarioLabel1: { loadZone: 'amazon:us:portland', percent: 25 },
+    scenarioLabel2: { loadZone: 'amazon:gb:london', percent: 25 },
+    scenarioLabel2: { loadZone: 'amazon:jp:tokyo', percent: 25 },
+    scenarioLabel2: { loadZone: 'amazon:au:sydney', percent: 25 },
+  },
   thresholds: {
     http_req_duration: ['p(95)<500'],
   },
 };
 
 export default function () {
-  const res = http.get('develop.delegate-market.nl');
+  const res = http.get('https://develop.delegate-market.nl');
   sleep(1);
 }
